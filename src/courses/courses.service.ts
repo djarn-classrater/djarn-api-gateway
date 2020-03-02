c34048c3324ac890b50565bfb0e6d8d6ca2c4bba
Wasutan Kitijerapat <markdog_@hotmail.com>
2020-03-03 00:02:24 +0700

    Update handle course error

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -21,6 +21,9 @@ export class CourseAPI extends RESTDataSource {
 
   async getCourse(course_id) {
     const response = await this.get(`course/${course_id}`)
+    if (!response.courseName) {
+      throw Error(`Don't have any course at ${course_id}`)
+    }
     return this.reducer({
       courseId: course_id,
       ...response,
